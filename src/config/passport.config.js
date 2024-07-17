285584dab01269c915b7ce0c41319033ea0ca1f9
aditya8850 <kalsotraaditya98@gmail.com>
2024-07-17 05:54:59 +0530

    "Update callbackURL in Google OAuth strategy from localhost to Render deployment URL"

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -42,7 +42,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: 'https://auth-template-nodejs-skill-test.onrender.com/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
